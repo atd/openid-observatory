fd81704b13b3acb52f815c48cd4fe6f2439ec41d
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2010-04-14 16:42:42 +0200

    Providers and discovery

--- app/models/uri.rb
+++ app/models/uri.rb
@@ -4,9 +4,13 @@ if defined?(ActiveRecord::Resource)
   require_dependency "#{ RAILS_ROOT }/vendor/plugins/station/app/models/uri"
 
   class Uri
-    Microformats = ["Adr", "HCard", "XFN", "RelLicense", "RelTag", "XOXO", "Geo", "VoteLinks"]
+    Providers = [ "myopenid.com", "pip.verisignlabs.com", "google.com", "aol.com", "wordpress.com", "livejournal.com", "claimid.com" ]
 
     has_one :uri_property, :dependent => :destroy
+
+    named_scope :provider, lambda { |p|
+      { :conditions => [ "uri LIKE ?", "%#{ p }%" ] }
+    }
 
     %w( foaf rss atom atompub rsd ).each do |p|
       eval <<-EOS
@@ -35,11 +39,79 @@ if defined?(ActiveRecord::Resource)
       }
     }
 
+    named_scope :html_discovery, lambda { |c|
+      conditions = case c
+                   when :both
+                     { "uri_properties.link_openid_server" => true,
+                       "uri_properties.link_openid2_provider" => true }
+                   when :only_1
+                     { "uri_properties.link_openid_server" => true,
+                       "uri_properties.link_openid2_provider" => false }
+                   when :only_2
+                     { "uri_properties.link_openid_server" => false,
+                       "uri_properties.link_openid2_provider" => true }
+                   when 1
+                     { "uri_properties.link_openid_server" => true }
+                   when 2
+                     { "uri_properties.link_openid2_provider" => true }
+                   when :any
+                     [ "uri_properties.link_openid_server = ? OR uri_properties.link_openid2_provider = ?", true, true ]
+                   else
+                     raise "Unknown option for links_openid scope"
+                   end
+
+      { :joins => :uri_property,
+        :conditions => conditions }
+    }
+
     named_scope :xrds_service_type, lambda { |m|
       { :joins => :uri_property,
         :conditions => [ "uri_properties.xrds_service_types LIKE ?", "%#{ m }%" ]
       }
     }
+
+    named_scope :xrds_discovery, lambda { |c|
+
+      # FIXME: DRY!
+      conditions = case c
+                   when :both
+                     [ "( uri_properties.xrds_service_types LIKE ? OR uri_properties.xrds_service_types LIKE ? ) AND uri_properties.xrds_service_types LIKE ?", "%#{ UriProperty::XrdsOpenIdUris[:v1_0] }%", "%#{ UriProperty::XrdsOpenIdUris[:v1_1] }%", "%#{ UriProperty::XrdsOpenIdUris[:v2_0] }%" ]
+                   when :only_1
+                     [ "( uri_properties.xrds_service_types LIKE ? OR uri_properties.xrds_service_types LIKE ? ) AND uri_properties.xrds_service_types NOT LIKE ?", "%#{ UriProperty::XrdsOpenIdUris[:v1_0] }%", "%#{ UriProperty::XrdsOpenIdUris[:v1_1] }%", "%#{ UriProperty::XrdsOpenIdUris[:v2_0] }%" ]
+                   when :only_2
+                     [ "uri_properties.xrds_service_types NOT LIKE ? AND uri_properties.xrds_service_types NOT LIKE ? AND uri_properties.xrds_service_types LIKE ?", "%#{ UriProperty::XrdsOpenIdUris[:v1_0] }%", "%#{ UriProperty::XrdsOpenIdUris[:v1_1] }%", "%#{ UriProperty::XrdsOpenIdUris[:v2_0] }%" ]
+                   when 1
+                     [ "uri_properties.xrds_service_types LIKE ? OR uri_properties.xrds_service_types LIKE ?", "%#{ UriProperty::XrdsOpenIdUris[:v1_0] }%", "%#{ UriProperty::XrdsOpenIdUris[:v1_1] }%" ]
+                   when 2
+                     [ "uri_properties.xrds_service_types LIKE ?", "%#{ UriProperty::XrdsOpenIdUris[:v2_0] }%" ]
+                   when :any
+                     [ "uri_properties.xrds_service_types LIKE ? OR uri_properties.xrds_service_types LIKE ? OR uri_properties.xrds_service_types LIKE ?", "%#{ UriProperty::XrdsOpenIdUris[:v1_0] }%", "%#{ UriProperty::XrdsOpenIdUris[:v1_1] }%", "%#{ UriProperty::XrdsOpenIdUris[:v2_0] }%" ]
+                   else
+                     raise "Unknown option for links_openid scope: #{ c }"
+                   end
+
+      { :joins => :uri_property,
+        :conditions => conditions }
+    }
+
+    named_scope :openid_version, lambda { |version|
+      conditions = case version
+                   when :both
+                     [ "( uri_properties.link_openid_server = ? AND uri_properties.link_openid2_provider = ? ) OR (( uri_properties.xrds_service_types LIKE ? OR uri_properties.xrds_service_types LIKE ? ) AND uri_properties.xrds_service_types LIKE ? )", true, true, "%#{ UriProperty::XrdsOpenIdUris[:v1_0] }%", "%#{ UriProperty::XrdsOpenIdUris[:v1_1] }%", "%#{ UriProperty::XrdsOpenIdUris[:v2_0] }%" ]
+                   when :only_1
+                     [ "( uri_properties.link_openid_server = ? OR uri_properties.xrds_service_types LIKE ? OR uri_properties.xrds_service_types LIKE ? ) AND uri_properties.link_openid2_provider = ? AND uri_properties.xrds_service_types NOT LIKE ?", true, "%#{ UriProperty::XrdsOpenIdUris[:v1_0] }%", "%#{ UriProperty::XrdsOpenIdUris[:v1_1] }%", false, "%#{ UriProperty::XrdsOpenIdUris[:v2_0] }%" ]
+                   when :only_2
+                     [ "( uri_properties.link_openid2_provider = ? OR uri_properties.xrds_service_types LIKE ? ) AND uri_properties.link_openid_server = ? AND uri_properties.xrds_service_types NOT LIKE ? AND uri_properties.xrds_service_types NOT LIKE ? ", true, "%#{ UriProperty::XrdsOpenIdUris[:v2_0] }%", false, "%#{ UriProperty::XrdsOpenIdUris[:v1_0] }%", "%#{ UriProperty::XrdsOpenIdUris[:v1_1] }%" ]
+                   when :any
+                     [ "uri_properties.link_openid_server = ? OR uri_properties.link_openid2_provider = ? OR uri_properties.xrds_service_types LIKE ? OR uri_properties.xrds_service_types LIKE ? OR uri_properties.xrds_service_types LIKE ?", true, true, "%#{ UriProperty::XrdsOpenIdUris[:v1_0] }%", "%#{ UriProperty::XrdsOpenIdUris[:v1_1] }%", "%#{ UriProperty::XrdsOpenIdUris[:v2_0] }%" ]
+                   else
+                     raise "Unknown option for openid_version scope: #{ version }"
+                   end
+      { :joins => :uri_property,
+        :conditions => conditions }
+    }
+
+
 
     # Rails Station REST magic
     acts_as_resource
@@ -52,6 +124,8 @@ if defined?(ActiveRecord::Resource)
       uri_property.rsd = html.rsd_links.any?
       uri_property.microformats = html.microformats.map{ |m| m.class.to_s.demodulize }.join(",")
       uri_property.xrds_service_types = xrds_service_types
+      uri_property.link_openid_server = html.openid_server_links?
+      uri_property.link_openid2_provider = html.openid2_provider_links?
       uri_property.save!
     end
 
--- app/models/uri_property.rb
+++ app/models/uri_property.rb
@@ -1,4 +1,33 @@
 class UriProperty < ActiveRecord::Base
+  Microformats = ["Adr", "HCard", "XFN", "RelLicense", "RelTag", "XOXO", "Geo", "VoteLinks"]
+
+  XrdsOpenIdUris = {
+    :v1_0 => "http://openid.net/signon/1.0",
+    :v1_1 => "http://openid.net/signon/1.1",
+    :v2_0 => "http://specs.openid.net/auth/2.0/signon"
+  }
+
+  XrdsServiceTypes = {
+    "http://openid.net/signon/1.0" => "OpenID Authentication 1.0",
+    "http://openid.net/signon/1.1" => "OpenID Authentication 1.1",
+    "http://openid.net/sreg/1.0"   => "OpenID Simple Registration Extension 1.0",
+    "http://openid.net/srv/ax/1.0" => "OpenID Attribute Exchange 1.0",
+    "http://openid.net/extensions/sreg/1.1" => "OpenID Simple Registration Extension 1.0",
+    "http://specs.openid.net/auth/2.0/server" => "OpenID Authentication 2.0 OP Identifier Element",
+    "http://specs.openid.net/auth/2.0/signon" => "OpenID Authentication 2.0 Claimed Identifier Element",
+    "http://specs.openid.net/extensions/pape/1.0" => "OpenID Provider Authentication Policy Extension 1.0",
+    "http://specs.openid.net/extensions/ui/1.0/mode/popup" => "OpenID User Interface Extension 1.0",
+    "http://specs.openid.net/extensions/ui/1.0/icon" => "OpenID User Interface Extension 1.0",
+    "http://specs.openid.net/extensions/oauth/1.0" => "OpenID OAuth Extension",
+    "http://schemas.openid.net/pape/policies/2007/06/phishing-resistant" => "OpenID PAPE Phishing-Resistant Authentication",
+    "http://schemas.openid.net/pape/policies/2007/06/multi-factor" => "OpenID PAPE Multi-Factor Authentication",
+    "http://schemas.openid.net/pape/policies/2007/06/multi-factor-physical" => "OpenID PAPE Physical Multi-Factor Authentication",
+    "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/privatepersonalidentifier" => "OpenID GSA Profile extension",
+    "http://www.idmanagement.gov/schema/2009/05/icam/no-pii.pdf" => "OpenID GSA Profile extension",
+    "http://www.idmanagement.gov/schema/2009/05/icam/openid-trust-level1.pdf" => "OpenID GSA Profile extension",
+    "http://csrc.nist.gov/publications/nistpubs/800-63/SP800-63V1_0_2.pdf" => "OpenID extension",
+  }
+
   belongs_to :uri
 
   serialize :xrds_service_types, Array
